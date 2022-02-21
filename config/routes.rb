08cd63972ea5051f8c97a315835ada197f70531b
Ryu <goskn88006@yahoo.co.jp>
2022-02-21 16:34:21 +0900

    top.html.erbを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'blogs#top'
+  resources :blogs do
+    collection do
+      get :top
+    end
+  end
 end
